b071a2e098b6944b578f881a2930d05809598e24
needhideshmukh <needhideshmukh05@gmail.com>
2024-06-06 14:08:57 +0530

    :zap: 3.0

--- admin.js
+++ admin.js
@@ -1,21 +1,42 @@
-// Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", () => {
-  // Ensure Browserify has loaded the modules
-  const submitIdentifier = window.submitIdentifier
-  const eventManager = window.eventManager
+document.getElementById("eventCreateButton").onclick = function () {
+  const eventName = document.getElementById("eventName").value
+  const guestCount = document.getElementById("guestCount").value
+  const guestEmail = document.getElementById("guestEmail").value
+  const adminHash = document.getElementById("adminHash").value
 
-  // Get references to the input fields and buttons
-  const logButton = document.getElementById("logButton")
-  const messageInput = document.getElementById("messageInput")
-  const guestCountInput = document.getElementById("guestCount")
-  const guestEmailInput = document.getElementById("guestEmail")
+  // Create JavaScript object with dynamic form values
+  const formData = {
+    email: guestEmail,
+    identifier: eventName,
+    document_hash: adminHash,
+    guestCount: guestCount,
+  }
 
-  // Add a click event listener to the log button
-  logButton.addEventListener("click", () => {
-    const identifier = messageInput.value
-    const guestCount = guestCountInput.value
-    const guestEmail = guestEmailInput.value
+  // Convert JavaScript object to JSON
+  const jsonData = JSON.stringify(formData)
 
-    submitIdentifier({ identifier, guestCount, guestEmail })
+  // Now you can use `jsonData` for whatever purpose you need
+  console.log(jsonData)
+
+  fetch("https://api.demo.dhiway.com/api/v1/dmp/create", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonData,
   })
-})
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok")
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log("Success:", data)
+      // Handle success response here
+    })
+    .catch((error) => {
+      console.error("Error:", error)
+      // Handle error here
+    })
+}
