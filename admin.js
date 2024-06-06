a6e80c6bea139bde571304a12ce59d6addd883d2
needhideshmukh <needhideshmukh05@gmail.com>
2024-06-06 11:22:42 +0530

    :zap: 2.0

--- admin.js
+++ admin.js
@@ -4,17 +4,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const submitIdentifier = window.submitIdentifier
   const eventManager = window.eventManager
 
-  // Get references to the input field and buttons
+  // Get references to the input fields and buttons
   const logButton = document.getElementById("logButton")
   const messageInput = document.getElementById("messageInput")
-  const guestCount = document.getElementById("guestCount")
-  const guestEmail = document.getElementById("guestEmail")
+  const guestCountInput = document.getElementById("guestCount")
+  const guestEmailInput = document.getElementById("guestEmail")
 
   // Add a click event listener to the log button
   logButton.addEventListener("click", () => {
     const identifier = messageInput.value
-    const guestCount = guestCount.value
-    const guestEmail = guestEmail.value
-    submitIdentifier(identifier)
+    const guestCount = guestCountInput.value
+    const guestEmail = guestEmailInput.value
+
+    submitIdentifier({ identifier, guestCount, guestEmail })
   })
 })
